d411e5328464cb1622449d984d6d28bebca487b0
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2017-10-14 02:49:22 +0200

    deleted prop, autcreate models from property names with /(_id)$/

--- src/classes/model.php
+++ src/classes/model.php
@@ -5,10 +5,22 @@ abstract class Model {
     use Traits\GetSet;
 
     protected $id;
+    protected $deleted;
 
     public function __construct($options = array()) {
         foreach($options as $key => $value) {
-            if(property_exists($this, $key)) $this->$key = $value;
+            if(preg_match('/([\w]+)_id$/', $key, $m)) {
+                $class_name = '\App\Models\\'.$m[1];
+                if(ClassManager::markedNotExisting($class_name) == false) {
+                    if(class_exists($class_name)) {
+                        $key = $m[1];
+                        $value = $class_name::grab($value);
+                    }
+                }
+            }
+            if(property_exists($this, $key)) {
+                $this->$key = $value;
+            }
         }
     }
 
@@ -19,12 +31,14 @@ abstract class Model {
     public function save() {
         $properties = get_object_vars($this);
 
-        foreach($properties as $name => $obj) {
-            if($obj instanceof \App\Model) {
-                if(!$obj->isCreated()) {
-                    $obj->save();
+        foreach($properties as $name => $value) {
+            if(is_null($value)) $properties[$name] = null;
+
+            if($value instanceof \App\Model) {
+                if(!$value->isCreated()) {
+                    $value->save();
                 }
-                $properties["{$name}_id"] = $obj->get('id');
+                $properties["{$name}_id"] = $value->get('id');
                 unset($properties[$name]);
             }
             elseif($name == 'id') {
@@ -34,20 +48,23 @@ abstract class Model {
 
         list($table_name, $self_class) = self::getTableName();
 
-        $query = new QueryBuilder\Builder($table_name, true);
+        $query = new QueryBuilder\Builder($table_name);
 
         if($this->isCreated()) {
-            $properties['stamp'] = 'NOW()'; // set last update date
-
-            if(!$query->where('id', '=', $this->id)->update($properties)) throw new \App\QueryBuilder\QueryBuilderException("could not update data: {$query->getError()}");
+            //$properties['stamp'] = 'NOW()'; // set last update date
+            $query->where('id', '=', $this->id)->update($properties);
         }
         else {
             $properties['user_id'] = 1;
             $properties['createDate'] = 'NOW()';
+            $properties['deleted'] = '0';
 
             $res = $query->insert($properties);
 
-            if(!$res) throw new \App\QueryBuilder\QueryBuilderException("could not insert data: {$query->getError()}");
+            if(!$res) {
+                throw new \App\QueryBuilder\QueryBuilderException("could not insert data: {$query->getError()}");
+            }
+
 
             $this->id = $query->lastInsertId();
         }
@@ -65,28 +82,49 @@ abstract class Model {
     }*/
 
     // Static methods.
-    public static function getOptions($value, $column = 'id', $all = false) {
+    public static function getOptions($filters = array(), $all = false) {
         list($table_name, $self_class) = self::getTableName();
 
         $query = new \App\QueryBuilder\Builder($table_name);
-        $query->where($column, '=', $value)->where('deleted', '=', '0');
+        $query->where('deleted', '=', '0')->orderBy('id', 'ASC');
+
         if(!$all) $query->limit(1);
 
+        if(is_array($filters)) {
+            foreach($filters as $filter) {
+                if(count($filter) != 3) continue;
+                $query->where($filter[0], $filter[1], $filter[2]);
+            }
+        }
+
         $res = $query->get();
+
         if(!empty($res)) {
             $options = $all ? $res : current($res);
             return array($options, $self_class);
         }
-        throw new \InvalidArgumentException("{$self_class} with {$column} = {$value} does not exists!");
+        throw new \InvalidArgumentException("No entries found for {$self_class}!");
     }
 
     public static function grab($value, $column = 'id') {
-        list($options, $self_class) = self::getOptions($value, $column);
+        list($options, $self_class) = self::getOptions(array(
+            array($column, '=', $value)
+        ));
         return new $self_class($options);
     }
 
-    public static function grabAll($value, $column = 'id') {
-        list($options, $self_class) = self::getOptions($value, $column, true);
+    public static function grabByFilter(array $filters) {
+        list($options, $self_class) = self::getOptions($filters, true);
+
+        $objs = array();
+        foreach($options as $o) {
+            $objs[] = new $self_class($o);
+        }
+        return $objs;
+    }
+
+    public static function grabAll() {
+        list($options, $self_class) = self::getOptions(array(), true);
 
         $objs = array();
         foreach($options as $o) {
@@ -96,7 +134,7 @@ abstract class Model {
     }
 
     public static function all() {
-        
+
     }
 
     public static function new() {
