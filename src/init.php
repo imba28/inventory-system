90ed7b1e9448b8d525b09dbd41530c2207a61343
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2017-10-14 02:48:03 +0200

    init routen

--- src/init.php
+++ src/init.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once('lib/functions.php');
 
 use App;
@@ -9,6 +13,17 @@ use App;
 \App\Configuration::set('DB_USER', 'root');
 \App\Configuration::set('DB_PWD', 'keins');
 \App\Configuration::set('DB_PREFIX', 'av');
+
+$router = App\Router::getInstance();
+$router->addRoute('all', 'p:(/$|/home$)', array('Controller', 'PageController', 'home'));
+$router->addRoute('all', 'p:(/product)', array('Controller', 'PageController', 'products'));
+$router->addRoute('all', 'customers', array('Controller', 'PageController', 'customers'));
+
+\App\Menu::getInstance()->set('items', array(
+    'Produkte' => 'products',
+    'Kunden' => 'customers',
+    'Inventur' => 'inventur'
+));
 
 try {
     \App\Registry::setDatabase(new \App\Database());
