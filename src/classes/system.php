121aa3c79f579c07b24d2b3449e4c696d88c9f73
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2017-10-14 02:46:39 +0200

    system status klasse

---
+++ src/classes/system.php
@@ -0,0 +1,68 @@
+<?php
+namespace App;
+
+final class System {
+    use Traits\Singleton;
+
+    private $status = array(
+        'error' => array(),
+        'success' => array(),
+        'warning' => array(),
+        'note' => array()
+    );
+
+    public function addMessage($type, $msg) {
+        $statusTypes = array_keys($this->status);
+
+        if(in_array($type, $statusTypes)) {
+            if(!in_array($msg, $this->status[$type])) {
+                $this->status[$type][] = $msg;
+            }
+        }
+        else throw new \InvalidArgumentException('Unknown type!');
+    }
+
+    private function getTitle($type) {
+        switch($type) {
+            case 'error':
+                return 'Fehler';
+            case 'success':
+                return 'Erfolg';
+            case 'warning':
+                return 'Warnung';
+            case 'note':
+                return 'Info';
+            default:
+                return null;
+        }
+    }
+
+    private function renderMessages($type, array $data) {
+        $html = '';
+
+        for($i = 0; $i < count($data); $i++) {
+            $msg = $data[$i];
+            $html .= "
+            <div class='alert alert-{$type} alert-dismissible fade show' role='alert'>
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                </button>
+                <strong>{$this->getTitle($type)}</strong> {$msg}
+            </div>";
+        }
+
+        return $html;
+    }
+
+    public static function getStatus($type = 'all') {
+        $html = '';
+        $instance = self::getInstance();
+
+        foreach($instance->status as $type => $data) {
+            $html .= $instance->renderMessages($type, $data);
+        }
+
+        return $html;
+    }
+}
+?>
