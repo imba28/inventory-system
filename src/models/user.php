9d4b28579d52ddb685f50b441d558d56aace829d
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2017-10-17 17:59:12 +0200

    model user

--- src/models/user.php
+++ src/models/user.php
@@ -1,7 +1,24 @@
 <?php
 namespace App\Models;
 
-class User extends Customer {
-    
+class User extends \App\Model {
+    protected $name;
+    protected $username;
+    protected $password;
+
+    public function __construct($options = array()) {
+        parent::__construct($options);
+
+        $this->on('save', function($e){
+            $user = $e->getContext();
+
+            $password = $user->get('password');
+            $salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
+            $salt = sprintf("$2a$%02d$", 10) . $salt;
+            $password = crypt($password, $salt);
+
+            $user->set('password', $password);
+        });
+    }
 }
 ?>
