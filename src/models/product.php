c1eb0873d5830ab5669ccbbf301abc694c752ab7
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2017-10-22 23:13:21 +0200

    Product Model kann Bilder laden

--- src/models/product.php
+++ src/models/product.php
@@ -9,6 +9,45 @@ class Product extends \App\Model {
     protected $condition;
     protected $note;
 
+    public function __construct($options = array()) {
+        parent::__construct($options);
+
+        if($this->isCreated()) {
+            try {
+                $this->images = ProductImage::grabByFilter(array(
+                    array('product_id', '=', $this->id)
+                ));
+            }
+            catch( \App\Exceptions\NothingFoundException $e) {
+                $this->images = array();
+            }
+        }
+    }
+
+    public function addImage(ProductImage $image) {
+        $this->images[] = $image;
+    }
+    public function getImages() {
+        return $this->images;
+    }
+
+    public function save($head_column = null, $head_id = null) {
+        if(!empty($this->images)) {
+            $images = $this->images;
+            unset($this->images);
+
+            try {
+                parent::save($head_column, $head_id);
+            }
+            catch(\App\QueryBuilder\NothingChangedException $e) {}
+
+            foreach($images as $image) {
+                $image->save('product_id', $this->getId());
+            }
+        }
+        else parent::save();
+    }
+
     public function isAvailable() {
         try {
             $action = \App\Models\Action::grabByFilter(array(
@@ -17,7 +56,7 @@ class Product extends \App\Model {
             ));
             return count($action) == 0;
         }
-        catch(\InvalidArgumentException $e) {
+        catch(\App\Exceptions\NothingFoundException $e) {
             return true;
         }
     }
