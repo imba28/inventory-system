3a153574325dcbd28331b5213af8f244f7febadc
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2017-11-22 23:48:31 +0100

    feat(controller): add basic controller class

---
+++ src/classes/basiccontroller.php
@@ -0,0 +1,44 @@
+<?php
+namespace App;
+
+abstract class BasicController {
+    protected $layout;
+    protected $response;
+    protected $view;
+
+    public function __construct($layout = 'default') {
+        $this->layout = $layout;
+        $this->response = new \App\HttpResponse();
+        $this->request = new \App\HttpRequest();
+        $this->view = new \App\View();
+
+        $this->view->assign('request', $this->request);
+    }
+
+    protected function renderContent() {
+        $this->response->append($this->getLayoutComponent('head'));
+        $this->response->append($this->view->render());
+        $this->response->append($this->getLayoutComponent('footer'));
+        $this->response->flush();
+    }
+
+    protected function bufferContent($path) {
+        ob_start();
+        include($path);
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
+
+    protected function getLayoutComponent($type = 'head') {
+        if(file_exists(ABS_PATH."/src/layouts/{$this->layout}-{$type}.php")) {
+            return $this->bufferContent(ABS_PATH."/src/layouts/{$this->layout}-{$type}.php");
+        }
+        throw new \InvalidArgumentException("Layout {$this->layout}-{$type}` does not exists!`");
+    }
+
+    abstract public function error($status);
+}
+
+?>
