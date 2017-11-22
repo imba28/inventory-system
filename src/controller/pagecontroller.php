a5f58c6962fda58bdcb49b2baeaab9b3aa25ffca
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2017-11-22 23:50:33 +0100

    refactor(pagecontroller): refactor controller

--- src/controller/pagecontroller.php
+++ src/controller/pagecontroller.php
@@ -1,20 +1,7 @@
 <?php
 namespace App\Controller;
 
-class PageController implements \App\Interfaces\Controller {
-    protected $layout;
-    protected $response;
-    protected $view;
-
-    public function __construct($layout = 'default') {
-        $this->layout = $layout;
-        $this->response = new \App\HttpResponse();
-        $this->request = new \App\HttpRequest();
-        $this->view = new \App\View();
-
-        $this->view->assign('request', $this->request);
-    }
-
+class PageController extends \App\BasicController implements \App\Interfaces\Controller {
     public function products() {
         if($this->request->issetParam('id')) {
             $product = \App\Models\Product::grab($this->request->getParam('id'));
@@ -79,46 +66,43 @@ class PageController implements \App\Interfaces\Controller {
                     $this->view->setTemplate('product-update');
                 }
                 elseif($this->request->getParam('sub') == 'rent') {
-                    try { // Check if already rented
-                        $action = \App\Models\Action::grabByFilter(array(
-                            array('product_id', '=', $product->get('id')),
+                    if(!$product->isAvailable()) {
+                        $action = current(\App\Models\Action::grabByFilter(array(
+                            array('product_id', '=', $product->getId()),
                             array('returnDate', 'IS', 'NULL')
-                        ));
-                        if(count($action) !== 0) {
-                            $this->view->setTemplate('product-rented');
-                            $this->view->assign('action', current($action));
-                            $this->renderContent();
-                            return;
-                        }
+                        )));
+
+                        $this->view->assign('action', $action);
+                        $this->view->setTemplate('product-rented');
                     }
-                    catch(\App\Exceptions\NothingFoundException $e) {}
+                    else {
+                        if($this->request->issetParam('submit')) {
+                            try {
+                                $customer = \App\Models\Customer::grab($this->request->getParam('internal_id'), 'internal_id');
+                            }
+                            catch(\App\Exceptions\NothingFoundException $e) {
+                                $customer = \App\Models\Customer::new();
+                                $customer->set('internal_id', $this->request->getParam('internal_id'));
+                            }
 
-                    if($this->request->issetParam('submit')) {
-                        try {
-                            $customer = \App\Models\Customer::grab($this->request->getParam('internal_id'), 'internal_id');
-                        }
-                        catch(\App\Exceptions\NothingFoundException $e) {
-                            $customer = \App\Models\Customer::new();
-                            $customer->set('internal_id', $this->request->getParam('internal_id'));
+                            $expectedReturnDate = !empty($this->request->getParam('expectedReturnDate')) ? $this->request->getParam('expectedReturnDate') : null;
+
+                            $action = \App\Models\Action::new();
+                            $action->set('product', $product);
+                            $action->set('customer', $customer);
+                            $action->set('rentDate', 'NOW()');
+                            $action->set('expectedReturnDate', $expectedReturnDate);
+
+                            if($action->save()) {
+                                \App\System::getInstance()->addMessage('success', 'Produkt verliehen!');
+                            }
+                            else {
+                                \App\System::getInstance()->addMessage('error', 'Produkt konnte nicht verliehen werden!');
+                            }
                         }
 
-                        $expectedReturnDate = !empty($this->request->getParam('expectedReturnDate')) ? $this->request->getParam('expectedReturnDate') : null;
-
-                        $action = \App\Models\Action::new();
-                        $action->set('product', $product);
-                        $action->set('customer', $customer);
-                        $action->set('rentDate', 'NOW()');
-                        $action->set('expectedReturnDate', $expectedReturnDate);
-
-                        if($action->save()) {
-                            \App\System::getInstance()->addMessage('success', 'Produkt verliehen!');
-                        }
-                        else {
-                            \App\System::getInstance()->addMessage('error', 'Produkt konnte nicht verliehen werden!');
-                        }
+                        $this->view->setTemplate('product-rent');
                     }
-
-                    $this->view->setTemplate('product-rent');
                 }
                 elseif($this->request->getParam('sub') == 'return') {
                     try {
@@ -139,18 +123,23 @@ class PageController implements \App\Interfaces\Controller {
                         }
 
                         if($this->request->issetParam('submit')) {
-                            if($this->request->issetParam('returnDate')) {
-                                $date = \DateTime::createFromFormat('d.m.Y', $this->request->getParam('returnDate'));
-                                if($date !== false) {
-                                    $action->returnProduct($date->format('Y-m-d H:i:s'));
-                                }
-                                else {
-                                    $action->returnProduct();
-                                }
+                            if($product->isAvailable()) {
+                                \App\System::getInstance()->addMessage('error', 'Produkt wurde bereits zurückgegeben!');
                             }
-                            else $action->returnProduct();
+                            else {
+                                if($this->request->issetParam('returnDate')) {
+                                    $date = \DateTime::createFromFormat('d.m.Y', $this->request->getParam('returnDate'));
+                                    if($date !== false) {
+                                        $action->returnProduct($date->format('Y-m-d H:i:s'));
+                                    }
+                                    else {
+                                        $action->returnProduct();
+                                    }
+                                }
+                                else $action->returnProduct();
 
-                            \App\System::getInstance()->addMessage('success', "{$product->get('name')} wurde erfolgreich zurückgegeben!");
+                                \App\System::getInstance()->addMessage('success', "{$product->get('name')} wurde erfolgreich zurückgegeben!");
+                            }
                         }
                     }
                     catch(\Exception $e) {
@@ -332,29 +321,6 @@ class PageController implements \App\Interfaces\Controller {
         $this->view->setTemplate('error');
 
         $this->renderContent();
-    }
-
-
-    private function renderContent() {
-        $this->response->append($this->getLayoutComponent('head'));
-        $this->response->append($this->view->render());
-        $this->response->append($this->getLayoutComponent('footer'));
-        $this->response->flush();
-    }
-
-    private function bufferContent($path) {
-        ob_start();
-        include($path);
-        $content = ob_get_contents();
-        ob_end_clean();
-
-        return $content;
-    }
-    private function getLayoutComponent($type = 'head') {
-        if(file_exists(ABS_PATH."/src/layouts/{$this->layout}-{$type}.php")) {
-            return $this->bufferContent(ABS_PATH."/src/layouts/{$this->layout}-{$type}.php");
-        }
-        throw new \InvalidArgumentException("Layout {$this->layout}-{$type}` does not exists!`");
     }
 }
 ?>
