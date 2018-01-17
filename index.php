61bb3e480d124b01a85b290dc1482faaa8af93f8
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-01-17 17:51:42 +0100

    style(index): remove comment

--- index.php
+++ index.php
@@ -3,20 +3,6 @@ require_once('vars.php');
 require_once('src/init.php');
 
 try {
-    /*$product = App\Models\Product::new();
-    $product->set('name','Playstation 3');
-    $product->set('type', 'Konsole');
-    $product->save();
-
-    $products = App\Models\Product::grabAll(1, 'user_id');
-
-    vd($products);
-
-    die();
-
-    $action = new App\Rental\Action($product, $customer);
-    $action->save();*/
-
     $router->route();
 }
 catch(\Exception $e) {
