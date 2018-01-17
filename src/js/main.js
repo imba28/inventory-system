81375280a85dfa4c0436b4706e43376eafddb11e
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-01-18 00:40:28 +0100

    fix(tinymce): remove fullpage plugin, remove forced br and p tags

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         selector: '.textarea',
         height: 500,
         theme: 'modern',
-        plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools linkchecker contextmenu colorpicker textpattern help',
+        plugins: 'print preview powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools linkchecker contextmenu colorpicker textpattern help',
         toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
         image_advtab: true,
         templates: [
@@ -18,6 +18,9 @@ $(document).ready(function(){
         content_css: [
         '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
         '//www.tinymce.com/css/codepen.min.css'
-        ]
+        ],
+        force_br_newlines : false,
+        force_p_newlines : false,
+        forced_root_block : ''
     });
 });
