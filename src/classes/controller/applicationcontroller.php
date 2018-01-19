5890e2fa1a613f8072b189e3b822c2a850d47b16
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-01-19 19:06:45 +0100

    fix(view): hide some content if user is not signed in

--- src/classes/controller/applicationcontroller.php
+++ src/classes/controller/applicationcontroller.php
@@ -4,6 +4,26 @@ namespace App\Controller;
 abstract class ApplicationController extends \App\BasicController {
     private $currentUser;
 
+    public function __construct($layout = 'default') {
+        parent::__construct($layout);
+
+        $this->view->assign('currentUser', $this->getCurrentUser());
+        $this->view->assign('isUserSignedIn', $this->isUserSignedIn());
+
+        if($this->isUserSignedIn()) {
+            \App\Menu::getInstance()->set('items', array(
+                'Produkte' => 'products',
+                'Kunden' => 'customers',
+                'Inventur' => 'inventur'
+            ));
+        }
+        else {
+            \App\Menu::getInstance()->set('items', array(
+                'Produkte' => 'products'
+            ));
+        }
+    }
+
     protected function getCurrentUser() {
         if(isset($_SESSION['user_id'])) {
             if(is_null($this->currentUser)) {
@@ -26,6 +46,12 @@ abstract class ApplicationController extends \App\BasicController {
 
     protected function redirectToRoute($route) {
         \App\Router::getInstance()->route($route);
+    }
+
+    protected function authenticateUser() {
+        if(!$this->isUserSignedIn()) {
+            $this->response->redirect('/');
+        }
     }
 }
 ?>
