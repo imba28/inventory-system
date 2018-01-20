26c4782984673781987b1035ab6cadfd470e62a5
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-01-20 17:26:24 +0100

    feat(view): add autocomplete for product categories in edit form

--- src/layouts/default-head.php
+++ src/layouts/default-head.php
@@ -12,7 +12,7 @@
     <link href="/src/css/bootstrap.datepicker.min.css" rel="stylesheet">
     <link href="/src/css/style.css" rel="stylesheet">
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="/src/js/bootstrap.min.js"></script>
     <script src="/src/js/bootstrap.datepicker.min.js"></script>
