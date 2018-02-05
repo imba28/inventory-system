32f701b0113236a980604ccc7722b2929591ea65
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-02-05 23:38:41 +0100

    feat(view): extract view data in response if amount of assigned data is one

--- src/classes/ViewJSON.php
+++ src/classes/ViewJSON.php
@@ -3,6 +3,8 @@ namespace App;
 
 class ViewJSON extends View {
     public function render($layout = null) {
+        if(count($this->data) == 1) return json_encode(current($this->data));
+        
         return json_encode($this->data);
     }
 
