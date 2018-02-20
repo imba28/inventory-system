d5a37570b3080e9d5e8a76a7919e70b1bb7e6aa1
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-02-20 21:06:44 +0100

    fix(controller): catch nothing found excetion when rendering list of customers

--- src/classes/Controller/CustomerController.php
+++ src/classes/Controller/CustomerController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controller;
 
+use App\Models\Customer;
+use App\System;
+
 class CustomerController extends ApplicationController
 {
     private $customer;
@@ -13,7 +16,7 @@ class CustomerController extends ApplicationController
 
         $this->beforeAction(array('show', 'update', 'delete', 'edit'), function ($params) {
             try {
-                $this->customer = \App\Models\Customer::find($params['id']);
+                $this->customer = Customer::find($params['id']);
                 $this->view->assign('customer', $this->customer);
             } catch (\App\Exceptions\NothingFoundException $e) {
                 $this->error(404);
@@ -23,7 +26,12 @@ class CustomerController extends ApplicationController
 
     public function index($params = array())
     {
-        $customers = \App\Models\Customer::all();
+        try {
+            $customers = Customer::all();
+        } catch (\App\Exceptions\NothingFoundException $e) {
+            $customers = array();
+        }
+        
         $this->view->assign('customers', $customers);
         $this->view->setTemplate('customers');
     }
@@ -33,7 +41,7 @@ class CustomerController extends ApplicationController
         $this->respondTo(function ($wants) {
             $wants->html(function () {
                 try {
-                    $rentalHistory = \App\Models\Action::findByFilter(array(
+                    $rentalHistory = Action::findByFilter(array(
                         array('customer', '=', $this->customer)
                     ), 10, array('returnDate' => 'ASC'));
                 } catch (\App\Exceptions\NothingFoundException $e) {
@@ -51,11 +59,11 @@ class CustomerController extends ApplicationController
         $this->customer->setAll($this->request->getParams());
 
         if (empty($this->customer->get('name')) || empty($this->customer->get('internal_id'))) {
-            \App\System::error('Name/FHS Nummer muss angegeben werden!');
+            System::error('Name/FHS Nummer muss angegeben werden!');
         } else {
             try {
                 $this->customer->save();
-                \App\System::success(
+                System::success(
                     "<a href='/customer/{$this->customer->getId()}'>
                         {$this->customer->get('name')}
                     </a>
@@ -64,13 +72,13 @@ class CustomerController extends ApplicationController
             } catch (\App\QueryBuilder\QueryBuilderException $e) {
                 list($error_code, $error_message, $error_value, $error_column) = $e->getData();
 
-                \App\System::error($error_message);
+                System::error($error_message);
             } catch (\App\QueryBuilder\NothingChangedException $e) {
-                //\App\System::info('Es wurde nichts geändert.');
+                //System::info('Es wurde nichts geändert.');
             } catch (\InvalidOperationException $e) {
-                \App\System::error('Fehler beim Speichern! ' . $e->getMessage());
+                System::error('Fehler beim Speichern! ' . $e->getMessage());
             } catch (\Exception $e) {
-                \App\System::error('Fehler beim Speichern!');
+                System::error('Fehler beim Speichern!');
             }
         }
 
@@ -92,9 +100,9 @@ class CustomerController extends ApplicationController
     public function delete()
     {
         if ($this->customer->remove()) {
-            \App\System::success('Kunde wurde gelöscht.');
+            System::success('Kunde wurde gelöscht.');
         } else {
-            \App\System::error('Es ist ein Fehler beim Löschen aufgetreten!');
+            System::error('Es ist ein Fehler beim Löschen aufgetreten!');
         }
 
         $this->index();
@@ -102,7 +110,7 @@ class CustomerController extends ApplicationController
 
     public function new()
     {
-        $this->customer = \App\Models\Customer::new();
+        $this->customer = Customer::new();
         $this->customer->set('user', $this->getCurrentUser());
         $this->view->assign('customer', $this->customer);
 
