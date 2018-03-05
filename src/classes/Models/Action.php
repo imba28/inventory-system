7b8bedf5ae4ac33995ae172c9548e075fef94250
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-03-05 03:35:50 +0100

    refactor(model): remove setter event listener from action model

--- src/classes/Models/Action.php
+++ src/classes/Models/Action.php
@@ -5,24 +5,6 @@ class Action extends Model
 {
     protected $attributes = ['product', 'customer', 'rentDate', 'returnDate', 'expectedReturnDate'];
 
-    protected function init()
-    {
-        $this->on(
-            'set',
-            function ($e) {
-                $property = $e->getInfo()['property'];
-                $value = $e->getInfo()['value'];
-
-                if (in_array($property, ['returnDate', 'expectedReturnDate'])) {
-                    $date = tryParseDate($value);
-                    if (!is_null($date)) {
-                        $this->data[$property] = $date->format('Y-m-d H:i:s');
-                    }
-                }
-            }
-        );
-    }
-
     public function isProductReturned()
     {
         return !is_null($this->get('returnDate'));
@@ -31,6 +13,6 @@ class Action extends Model
     public function returnProduct($date = 'NOW')
     {
         $this->set('returnDate', $date);
-        $this->save();
+        return $this->save();
     }
 }
